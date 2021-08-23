b2cf5aab93e5ef523b3c4c22c0bdb311b1477ffb
DhruvMitna19 <dhruvmitna9@gmail.com>
2021-08-23 09:57:05 +0530

    initial commit

---
+++ c#/helloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace helloWorld {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\nPress enter to exit...");
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
