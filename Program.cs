09ae0494cc79149541b2c1c69b81a4d876dfa553
DhruvMitna19 <dhruvmitna9@gmail.com>
2021-08-25 12:53:52 +0530

    reorganized folders

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace helloWorld {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Console.Write("Hello World!");
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
